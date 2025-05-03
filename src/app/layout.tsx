84da7bc52026df6703d9d04c7b2d8b6b2fc8f9ec
sailingsam <jainsaksham1004@gmail.com>
2025-05-03 22:35:49 +0530

    done with basic tailwind setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
   subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  display: "swap",
+  fallback: ["system-ui"],
+  variable: "--font-inter",
 });
 
 export const metadata: Metadata = {
   title: "ProjexBio",
   description: "Empowering Projects Across Campuses",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" },
+      { url: "/favicon-16x16.png", type: "image/png" },
+      { url: "/favicon-32x32.png", type: "image/png" },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -24,11 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+      <body className={`${inter.variable} antialiased`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start"></main>
-    </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold bg-linear-to-r from-brand-purple to-brand-blue">
+        Welcome to ProjexBio
+      </h1>
+      <p className="mt-4 text-lg">Empowering Projects Across Campuses</p>
+    </main>
   );
 }
