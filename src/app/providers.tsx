7cc27da48c8077c3d64ad7432bfba4f5290662fb
sailingsam <jainsaksham1004@gmail.com>
2025-06-26 02:29:25 +0530

    added tanstack libraries

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,15 +5,33 @@ import { AuthProvider } from "@/contexts/AuthContext";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { HeroUIProvider } from "@heroui/react";
 import { ThemeProvider } from "next-themes";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: 1000 * 60 * 15, // 15 minutes
+        gcTime: 1000 * 60 * 20, // 20 minutes
+        refetchOnWindowFocus: false,
+        refetchOnMount: false,
+        refetchOnReconnect: true,
+        retryOnMount: true,
+        retry: 2,
+      },
+    },
+  });
   return (
-    <HeroUIProvider>
-      <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
-        <AntdRegistry>
-          <AuthProvider>{children}</AuthProvider>
-        </AntdRegistry>
-      </ThemeProvider>
-    </HeroUIProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <HeroUIProvider>
+        <ThemeProvider attribute="class" defaultTheme="dark" enableSystem>
+          <AntdRegistry>
+            <AuthProvider>{children}</AuthProvider>
+          </AntdRegistry>
+        </ThemeProvider>
+      </HeroUIProvider>
+    </QueryClientProvider>
   );
 }
